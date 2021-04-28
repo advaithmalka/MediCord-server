54e2a01b2d7042c75a27b32a659c8d3be7dbf7da
AdvaithM26 <65194982+AdvaithM26@users.noreply.github.com>
2021-04-28 18:00:27 -0400

    deploy

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ const mongoose = require("mongoose");
 const router = require("./router");
 const app = express();
 const port = process.env.PORT || 6942;
-
 mongoose
   .connect(process.env.MONGO_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log(`Server running at http://localhost:${port}`))
-  .catch((err) => {
+  .catch(err => {
     console.error(err);
   });
 
+const allowList = ["https://advaithmalka.github.io", "http://localhost:3000"];
+app.use((req, res, next) => {
+  if (allowList.indexOf(req.headers.origin) === -1) return res.sendStatus(401);
+  next();
+});
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static("public"));
 app.use(router);
--- router.js
+++ router.js
@@ -1,23 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const router = express.Router();
-const { handleSignup, handleLogin } = require("./handlers");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User");
-const fs = require("fs");
 const keywords = require("./keywords");
 const fetch = require("node-fetch");
 const unirest = require("unirest");
 
 let SummarizerManager = require("node-summarizer").SummarizerManager;
-
-router.route("/signup").get(async ({ query }, res) => {
-  const hashPwd = await bcrypt.hash(query.password, 10);
-
+router.route("/").get((req, res) => {
+  res.sendStatus(200);
+});
+router.route("/signup").post(async ({ body }, res) => {
+  const hashPwd = await bcrypt.hash(body.password, 10);
   const user = new User({
     password: hashPwd,
-    email: query.email,
+    email: body.email,
   });
 
   const result = await user.save();
@@ -32,13 +31,13 @@ router.route("/signup").get(async ({ query }, res) => {
 
   res.send({ status: "ok", token });
 });
-router.route("/login").get(async ({ query }, res) => {
-  const user = await User.findOne({ email: query.email });
+router.route("/login").post(async ({ body }, res) => {
+  const user = await User.findOne({ email: body.email });
   if (!user) {
     return res.send({ error: "NOT_FOUND" });
   }
 
-  const validPwd = await bcrypt.compare(query.password, user.password);
+  const validPwd = await bcrypt.compare(body.password, user.password);
 
   if (!validPwd) {
     return res.send({ error: "INVALID_PASSWORD" });
@@ -69,24 +68,25 @@ router.route("/sessions/all/:userID").get(async (req, res) => {
   return res.send({ error: "NOT_FOUND" });
 });
 let lastQuery = "";
-router.route("/upload").get(async (req, res) => {
-  let url = req.query.url.replace(/\/(?=[^\/]*$)/, "%2F");
+router.route("/upload").post(async (req, res) => {
+  // let url = req.body.url.replace(/\/(?=[^\/]*$)/, "%2F");
+  let url = req.body.url;
   if (lastQuery === url) return;
   lastQuery = url;
   const postReq = unirest("POST", `https://api.assemblyai.com/v2/transcript`);
 
   postReq.headers({
-    authorization: "4ea3ef3cfc8147bf92058044dce13ef0",
+    authorization: process.env.ASSEMBLY_AI_KEY,
     "content-type": "application/json",
   });
 
   postReq.type("json");
   postReq.send({
-    audio_url: `${url}&token=${req.query.token}`,
+    audio_url: `${url}&token=${req.body.token}`,
     word_boost: keywords,
   });
 
-  postReq.end(async (re) => {
+  postReq.end(async re => {
     let data = await getTranscript(re);
     const poll = async () => {
       data = await getTranscript(re);
@@ -95,15 +95,15 @@ router.route("/upload").get(async (req, res) => {
 
         //Summarizer
         const sentences = data.text.match(/[\w|\)][.?!](\s|$)/g).length;
-        const numSentences = Math.ceil(sentences * (0.01 * req.query.percent));
+        const numSentences = Math.ceil(sentences * (0.01 * req.body.percent));
         let Summarizer = new SummarizerManager(data.text, numSentences);
         let summary = Summarizer.getSummaryByFrequency().summary;
 
-        const user = await User.findById(req.query.id);
+        const user = await User.findById(req.body.id);
         user.sessions.unshift({
           transcription: data.text,
           summary,
-          title: req.query.title,
+          title: req.body.title,
         });
         await user.save();
         res.send({ status: "ok" });
@@ -121,13 +121,13 @@ router.route("/upload").get(async (req, res) => {
 });
 
 module.exports = router;
-const getTranscript = async (re) => {
+const getTranscript = async re => {
   const r = await fetch(
     `https://api.assemblyai.com/v2/transcript/${re.body.id}`,
     {
       method: "GET",
       headers: {
-        authorization: "4ea3ef3cfc8147bf92058044dce13ef0",
+        authorization: process.env.ASSEMBLY_AI_KEY,
         "content-type": "application/json",
       },
     }
